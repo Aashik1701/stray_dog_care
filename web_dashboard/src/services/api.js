959788e6fa1fbcf36a71d950fed2e7503b27da52
Aashik1701 <md1704ashik@gmail.com>
2025-09-15 23:53:16 +0530

    update

--- web_dashboard/src/services/api.js
+++ web_dashboard/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 class ApiService {
   constructor() {
@@ -27,7 +27,7 @@ class ApiService {
   }
 
   async request(endpoint, options = {}) {
-    const url = `${API_BASE_URL}${endpoint}`;
+    const url = `${API_BASE_URL}/api${endpoint}`;
     const config = {
       headers: this.getHeaders(),
       ...options,
